d5bb8daf8679850b308ae02020e56c4d2152fd44
petrlr14 <pedrodeveloper14@gmail.com>
2021-04-18 16:07:01 -0600

    ✨ implement jwt

---
+++ src/user/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { JwtPayload } from './jwtPayload.interface';
+import { UserRepository } from './user.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository)
+    private readonly userRepository: UserRepository,
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate({ email }: JwtPayload) {
+    const user = await this.userRepository.findOne({ email });
+    if (!user) throw new UnauthorizedException();
+    return user;
+  }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SendgridModule } from '../sendgrid/sendgrid.module';
 import { FavoritesRoom } from './favoritesRooms.entity';
-import { UserController } from './user.controller';
+import { JwtStrategy } from './jwt.strategy';
 import { UserRepository } from './user.repository';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository, FavoritesRoom])],
-  controllers: [UserController],
-  providers: [UserService, UserResolver],
+  imports: [
+    SendgridModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRE') },
+      }),
+    }),
+    TypeOrmModule.forFeature([UserRepository, FavoritesRoom]),
+  ],
+  providers: [UserService, UserResolver, JwtStrategy],
+  exports: [JwtStrategy, PassportModule, TypeOrmModule],
 })
 export class UserModule {}
