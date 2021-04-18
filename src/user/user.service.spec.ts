f3654596063a2c3e1b89ae87d4ba02ca69707ba6
petrlr14 <pedrodeveloper14@gmail.com>
2021-04-18 16:08:22 -0600

    ✅ add get user by id test

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,11 +1,14 @@
 import { ConflictException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { SENDGRID_MAIL } from '../sendgrid/sendgrid.constants';
+import { SendgridService } from '../sendgrid/sendgrid.service';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 
 const mockRepository = () => ({
   createUser: jest.fn(),
+  getUser: jest.fn(),
 });
 
 describe('UserService', () => {
@@ -17,6 +20,13 @@ describe('UserService', () => {
       providers: [
         UserService,
         { provide: UserRepository, useFactory: mockRepository },
+        SendgridService,
+        {
+          provide: SENDGRID_MAIL,
+          useValue: {
+            send: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
@@ -45,6 +55,23 @@ describe('UserService', () => {
     it('should create user', async () => {
       jest.spyOn(repository, 'createUser').mockResolvedValue(user);
       const result = await service.createUser({ email: user.email });
+      expect(result.email).toBe(user.email);
+    });
+  });
+  describe('Get user by email', () => {
+    const user = new User();
+    user.id = 1;
+    it('should fail', async () => {
+      jest.spyOn(repository, 'getUser').mockRejectedValue(null);
+      try {
+        await service.getUser({ email: '123@gmail.com' });
+      } catch (e) {
+        expect(e).toBeNull();
+      }
+    });
+    it('should getUser', async () => {
+      jest.spyOn(repository, 'getUser').mockResolvedValue(user);
+      const result = await service.getUser({ email: user.email });
       expect(result.email).toBe(user.email);
     });
   });
