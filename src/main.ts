856403684be5005b128b3b3a7ecfa96145bd5ea1
petrlr14 <pedrodeveloper14@gmail.com>
2021-04-19 15:05:36 -0600

    🔧 add deploy configuration

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
+  app.enableCors();
   await app.listen(configService.get<number>('PORT') || 3000);
 }
 bootstrap();
