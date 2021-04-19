51d01c3698ba70f78da2c2a1d0f685f70431b856
petrlr14 <pedrodeveloper14@gmail.com>
2021-04-18 20:50:47 -0600

    ✨ add optional jwt guard

--- src/user/user.guard.ts
+++ src/user/user.guard.ts
@@ -5,12 +5,22 @@ import {
 } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
-
 @Injectable()
 export class GqlAuthGuard extends AuthGuard('jwt') {
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
     return ctx.getContext().req;
+  }
+}
+@Injectable()
+export class GqlOptionalAuthGuard extends AuthGuard('jwt') {
+  getRequest(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context);
+    return ctx.getContext().req;
+  }
+
+  handleRequest<User>(error: any, user: User, info: any) {
+    return user ?? null;
   }
 }
 
